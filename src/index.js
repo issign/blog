ef722ad9ac41d32bc453470cbf22186e0dc340bb
KihoLee <kiho2734@naver.com>
2025-06-01 18:54:36 +0900

    Modify setup and add tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 const el = document.getElementById("root");
 
 function App() {
-  return <h1>Hi</h1>;
+  return <div className="text-9xl">Hi</div>;
 }
 
 ReactDOM.createRoot(el).render(<App />);
