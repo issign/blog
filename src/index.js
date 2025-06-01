d70bf226d1c1851d0dd2d46333af92a9d0e72c1c
KihoLee <kiho2734@naver.com>
2025-06-01 19:07:24 +0900

    Add folders and Link file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const el = document.getElementById("root");
-
-function App() {
-  return <div className="text-9xl">Hi</div>;
-}
 
 ReactDOM.createRoot(el).render(<App />);
