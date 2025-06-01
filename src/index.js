f3f486852bbf1b2831de3bf6ed3ad179251e02dc
KihoLee <kiho2734@naver.com>
2025-06-01 18:31:43 +0900

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const el = document.getElementById("root");
+
+function App() {
+  return <h1>Hi</h1>;
+}
+
+ReactDOM.createRoot(el).render(<App />);
