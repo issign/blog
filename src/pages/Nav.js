45bd5378d75f57b7e60cfc8c30c78cca9abff742
KihoLee <kiho2734@naver.com>
2025-06-05 18:32:46 +0900

    Add fetchPosts function

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Dropdown from "../components/forms/Dropdown";
 
 function Nav() {
-  return <div>{}</div>;
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:3001/posts");
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const categories = new Set(posts.map((post) => post.category));
+  const renderedPosts = Array.from(categories).map((category, i) => {
+    const relatedPosts = posts.filter((post) => post.category == category);
+    return (
+      <Dropdown names={relatedPosts} key={i}>
+        {category}
+      </Dropdown>
+    );
+  });
+
+  return (
+    <div>
+      <Dropdown>소개</Dropdown>
+      {renderedPosts}
+    </div>
+  );
 }
 
 export default Nav;
