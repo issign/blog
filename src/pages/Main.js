65d22b809080b5ba314547dbbd1f690bf322d0f2
KihoLee <kiho2734@naver.com>
2025-06-11 18:28:39 +0900

    Add Create component

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
 import Route from "../components/links/Route";
+import Create from "./Create";
 
 function Main() {
   return (
     <div>
       <Route path="/create">
+        <Create></Create>
       </Route>
     </div>
   );
