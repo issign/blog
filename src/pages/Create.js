ea686644f844cb39490873c59ce9e3b6b6148c90
KihoLee <kiho2734@naver.com>
2025-06-12 13:54:09 +0900

    Add react-quill-new library

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,11 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill-new"; 
+import "react-quill-new/dist/quill.snow.css"; 
+
 function Create() {
-  return <div>create</div>;
+  const [value, setValue] = useState("");
+
+  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
 }
 
 export default Create;
