10f5bee6d5a199e8d123e52e45ba3530fa5a7d89
KihoLee <kiho2734@naver.com>
2025-06-24 18:25:57 +0900

    Modify Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Nav from "./pages/Nav";
 import Main from "./pages/Main";
+import Route from "./components/links/Route";
+import Create from "./pages/Create";
 
 function App() {
   return (
     <div className="bg-primary-color text-eclipse px-20">
-      <Nav />
-      <Main />
+      <Route path="/">
+        <Nav />
+        <Main />
+      </Route>
+      <Route path="/create">
+        <Create />
+      </Route>
     </div>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,10 @@
 import Route from "../components/links/Route";
-import Create from "./Create";
 import Sidebar from "../components/navigations/Sidebar";
 
 function Main() {
   return (
     <div className="pt-20">
       <Sidebar></Sidebar>
-      <Route path="/create">
-        <Create></Create>
-      </Route>
     </div>
   );
 }
